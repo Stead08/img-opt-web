1f87f57765e1bb50e2b49a75e29527e40419ff58
Stead08 <62312581+Stead08@users.noreply.github.com>
2024-12-22 12:06:56 +0900

    feat (*): losslessもwasmで追加

--- packages/wasm/pkg/webp.wasm.d.ts
+++ packages/wasm/pkg/webp.wasm.d.ts
@@ -1,37 +1,33 @@
 // TypeScript bindings for emscripten-generated code.  Automatically generated at compile time.
 declare namespace RuntimeExports {
-	/**
-	 * @param {string=} returnType
-	 * @param {Array=} argTypes
-	 * @param {Object=} opts
-	 */
-	function cwrap(
-		ident: any,
-		returnType?: string | undefined,
-		argTypes?: any[] | undefined,
-		opts?: any | undefined
-	): any;
-	let HEAPF32: any;
-	let HEAPF64: any;
-	let HEAP_DATA_VIEW: any;
-	let HEAP8: any;
-	let HEAPU8: any;
-	let HEAP16: any;
-	let HEAPU16: any;
-	let HEAP32: any;
-	let HEAPU32: any;
-	let HEAP64: any;
-	let HEAPU64: any;
+    /**
+     * @param {string=} returnType
+     * @param {Array=} argTypes
+     * @param {Object=} opts
+     */
+    function cwrap(ident: any, returnType?: string | undefined, argTypes?: any[] | undefined, opts?: any | undefined): any;
+    let HEAPF32: any;
+    let HEAPF64: any;
+    let HEAP_DATA_VIEW: any;
+    let HEAP8: any;
+    let HEAPU8: any;
+    let HEAP16: any;
+    let HEAPU16: any;
+    let HEAP32: any;
+    let HEAPU32: any;
+    let HEAP64: any;
+    let HEAPU64: any;
 }
 interface WasmModule {
-	_version(): number;
-	_create_buffer(_0: number, _1: number): number;
-	_destroy_buffer(_0: number): void;
-	_encode(_0: number, _1: number, _2: number, _3: number): void;
-	_free_result(_0: number): void;
-	_get_result_pointer(): number;
-	_get_result_size(): number;
+  _version(): number;
+  _create_buffer(_0: number, _1: number): number;
+  _destroy_buffer(_0: number): void;
+  _encode(_0: number, _1: number, _2: number, _3: number): void;
+  _lossless_encode(_0: number, _1: number, _2: number): void;
+  _free_result(_0: number): void;
+  _get_result_pointer(): number;
+  _get_result_size(): number;
 }
 
 export type MainModule = WasmModule & typeof RuntimeExports;
-export default function MainModuleFactory(options?: unknown): Promise<MainModule>;
+export default function MainModuleFactory (options?: unknown): Promise<MainModule>;
--- packages/wasm/webp.c
+++ packages/wasm/webp.c
@@ -5,50 +5,60 @@
 int result[2];
 
 EMSCRIPTEN_KEEPALIVE
-int version() {
+int version()
+{
   return WebPGetEncoderVersion();
 }
 
 EMSCRIPTEN_KEEPALIVE
-uint8_t* create_buffer(int width, int height) {
-  if (width <= 0 || height <= 0 || 
-      width > 16384 || height > 16384) {
+uint8_t *create_buffer(int width, int height)
+{
+  if (width <= 0 || height <= 0 ||
+      width > 16384 || height > 16384)
+  {
     return NULL;
   }
-  
+
   size_t size;
   if (__builtin_mul_overflow(width, height, &size) ||
-      __builtin_mul_overflow(size, 4, &size)) {
+      __builtin_mul_overflow(size, 4, &size))
+  {
     return NULL;
   }
-  
-  uint8_t* buffer = (uint8_t*)calloc(1, size);
-  if (!buffer) {
+
+  uint8_t *buffer = (uint8_t *)calloc(1, size);
+  if (!buffer)
+  {
     return NULL;
   }
-  
+
   return buffer;
 }
 
 EMSCRIPTEN_KEEPALIVE
-void destroy_buffer(uint8_t* p) {
-  if (p) {
+void destroy_buffer(uint8_t *p)
+{
+  if (p)
+  {
     free(p);
   }
 }
 
 EMSCRIPTEN_KEEPALIVE
-void encode(uint8_t* img_in, int width, int height, float quality) {
-  if (!img_in || width <= 0 || height <= 0) {
+void encode(uint8_t *img_in, int width, int height, float quality)
+{
+  if (!img_in || width <= 0 || height <= 0)
+  {
     result[0] = 0;
     result[1] = 0;
     return;
   }
 
-  uint8_t* img_out = NULL;
+  uint8_t *img_out = NULL;
   size_t size = WebPEncodeRGBA(img_in, width, height, width * 4, quality, &img_out);
 
-  if (size == 0 || !img_out) {
+  if (size == 0 || !img_out)
+  {
     result[0] = 0;
     result[1] = 0;
     return;
@@ -59,18 +69,46 @@ void encode(uint8_t* img_in, int width, int height, float quality) {
 }
 
 EMSCRIPTEN_KEEPALIVE
-void free_result(uint8_t* result) {
-  if (result) {
+void lossless_encode(uint8_t *img_in, int width, int height)
+{
+  if (!img_in || width <= 0 || height <= 0)
+  {
+    result[0] = 0;
+    result[1] = 0;
+    return;
+  }
+
+  uint8_t *img_out = NULL;
+  size_t size = WebPEncodeLosslessRGBA(img_in, width, height, width * 4, &img_out);
+
+  if (size == 0 || !img_out)
+  {
+    result[0] = 0;
+    result[1] = 0;
+    return;
+  }
+
+  result[0] = (int)img_out;
+  result[1] = size;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void free_result(uint8_t *result)
+{
+  if (result)
+  {
     WebPFree(result);
   }
 }
 
 EMSCRIPTEN_KEEPALIVE
-int get_result_pointer() {
+int get_result_pointer()
+{
   return result[0];
 }
 
 EMSCRIPTEN_KEEPALIVE
-int get_result_size() {
+int get_result_size()
+{
   return result[1];
 }
